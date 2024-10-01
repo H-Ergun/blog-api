44e559ed585c5195b082d471d37c43535f1d59bc
huseyin.ergun <huseyin.ergun@fintiya.com>
2024-10-01 23:07:15 +0300

    ignore

--- TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
+++ TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c3bbbb28cb4a217a3e9de43c56b9617bf89b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6f16fb0221bccdc5d953736b43caa3ff49faf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
