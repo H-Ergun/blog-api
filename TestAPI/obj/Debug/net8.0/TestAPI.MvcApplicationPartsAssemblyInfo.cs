55c003d80e039f3e95fe120aeb3ae8f0830c8a8a
huseyin.ergun <huseyin.ergun@fintiya.com>
2024-10-01 23:05:23 +0300

    buid

--- TestAPI/obj/Debug/net8.0/TestAPI.MvcApplicationPartsAssemblyInfo.cs
+++ TestAPI/obj/Debug/net8.0/TestAPI.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c3bbbb28cb4a217a3e9de43c56b9617bf89b4b")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
 
